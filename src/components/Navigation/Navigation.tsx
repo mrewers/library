0753ca491b4238ec3cc8704ee8a3083a65e3873a
Marek Rewers <mrewers@users.noreply.github.com>
2020-07-08 21:06:48 -0400

    Add nav element

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,16 @@
+import { h } from 'preact';
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/add">Add New</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navigation;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import { Fragment, h, render } from 'preact';
 import Router from 'preact-router';
 
+import Navigation from './components/Navigation/Navigation';
+
 import Books from './components/Pages/Books';
 import Input from './components/Pages/Input';
 
 const App = () => (
   <Fragment>
     <h1>Hello from Preact and Typescript!</h1>
+    <Navigation />
     <Router>
       <Books path="/" />
       <Input path="/add" />
