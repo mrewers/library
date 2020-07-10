0c1a23c9572c743646eee8cc07dec7656e524496
Marek Rewers <mrewers@users.noreply.github.com>
2020-07-09 22:42:55 -0400

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: [
+    '@cryptopapers/eslint-config',
+    '@cryptopapers/eslint-config/react',
+    '@cryptopapers/eslint-config/typescript',
+  ],
+  rules: {
+    // preact
+    'react/no-unknown-property': ['error', { ignore: ['class', 'for'] }],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        assert: 'either',
+        depth: 3,
+      },
+    ],
+    // typescript
+    '@typescript-eslint/strict-boolean-expressions': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: 'class', format: ['PascalCase'] },
+      { selector: 'interface', format: ['PascalCase'], prefix: ['I'] },
+      { selector: 'typeAlias', format: ['PascalCase'], prefix: ['Type'] },
+      { selector: 'variable', format: ['camelCase'] },
+      { selector: 'variable', types: ['function'], format: ['camelCase', 'PascalCase'] },
+    ],
+  },
+  settings: {
+    react: {
+      pragma: 'h',
+      version: '16.0',
+    },
+  },
+};
