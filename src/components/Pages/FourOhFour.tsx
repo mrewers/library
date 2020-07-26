473a23a8a167eb7657e096b450ec7f336d3cc25b
Marek Rewers <mrewers@users.noreply.github.com>
2020-07-25 22:46:33 -0400

    Add 404 page

---
+++ src/components/Pages/FourOhFour.tsx
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+
+const FourOhFour = (): h.JSX.Element => (
+  <div>
+    <h2>404: Page Not Found</h2>
+  </div>
+);
+
+FourOhFour.displayName = 'FourOhFour';
+
+export default FourOhFour;
