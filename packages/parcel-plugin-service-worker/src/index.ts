649b12c8132cf8bdaeba0b1b5dcf861e3f922357
Marek Rewers <mrewers@users.noreply.github.com>
2020-08-25 21:52:56 -0400

    Convert function into a thenable

--- packages/parcel-plugin-service-worker/src/index.ts
+++ packages/parcel-plugin-service-worker/src/index.ts
@@ -6,19 +6,18 @@ import ParcelBundler from 'parcel-bundler';
 import addScriptTag from './write-script-tag';
 
 module.exports = (bundler: ParcelBundler): void => {
-  const outDir = bundler?.options?.outDir as string;
+  const outDir = bundler.options.outDir as string;
 
   bundler.on('buildEnd', () => {
+    const index = path.resolve(outDir, 'index.html');
+
     workBox
       .generateSW({
         globDirectory: 'dist',
         globPatterns: ['**/*.{css,js,html,ico,jpg,jpeg,png,svg,webmanifest}'],
         swDest: 'dist/sw.js',
       })
-      .catch(err => console.error(err));
-
-    const index = path.resolve(outDir, 'index.html');
-
-    addScriptTag(index);
+      .catch(err => console.error(`Error: ${err}`))
+      .then(() => addScriptTag(index));
   });
 };
