370f59afaae494e200017f6dbe6bd54c242ef3f5
mrewers <hello@marek.dev>
2023-07-25 12:24:49 -0400

    feat: add some flare to the 404 page

--- src/components/Pages/FourOhFour.tsx
+++ src/components/Pages/FourOhFour.tsx
@@ -1,11 +1,17 @@
 import type { Component } from 'solid-js';
 
 import Layout from 'components/Layout/Layout';
+import gifUrl from '../../../public/assets/public/to-the-library.gif'
+
+import s from './Pages.module.scss';
 
 const FourOhFour: Component = () => (
-  <Layout>
+  <Layout stats={false}>
     <div>
-      <h1>404: Page Not Found</h1>
+      <h1 class={s.subhead}>404: Page Not Found</h1>
+      <figure class={s.gif}>
+        <img src={gifUrl} />
+      </figure>
     </div>
   </Layout>
 );
