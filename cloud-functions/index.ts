62674c047f964f0e7b81808b30ee65b86771ebf1
mrewers <hello@marek.dev>
2024-01-19 17:08:56 -0500

    feat: clean up previous abortive efforts at serverless functions

--- cloud-functions/index.ts
+++
@@ -1,7 +0,0 @@
-import getBooks from './books/getBooks';
-import createBook from './book/createBook';
-
-module.exports = {
-  createBook,
-  getBooks
-}
--- functions/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { bookRoutes } from './books';
-import { readerRoutes } from './readers';
-import { retiredRoutes } from './retired';
-
-export const routes = {
-  bookRoutes,
-  readerRoutes,
-  retiredRoutes,
-};
