ad0089829da4bd17eab5b64c6bbdb64d0d673d9b
mrewers <hello@marek.dev>
2024-01-19 16:15:23 -0500

    feat: set to make each function its own module

--- serverless/books/get/main.go
+++ serverless/books/get/main.go
@@ -1,36 +1,42 @@
-package main
+package getbooks
 
 import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"cloud.google.com/go/firestore"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
 
-func getBooks() []*firestore.DocumentSnapshot {
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	var books []*firestore.DocumentSnapshot
+
 	ctx := context.Background()
 
 	client, err := firestore.NewClient(ctx, os.Getenv("PROJECT_ID"))
 
 	if err != nil {
 		log.Print("Error connecting to firestore")
+		fmt.Fprint(w, "Failed to connect to firestore")
+		return
 	}
 
 	defer client.Close()
 
-	books, err := client.GetAll(ctx, []*firestore.DocumentRef{client.Doc("Books")})
+	books, err = client.GetAll(ctx, []*firestore.DocumentRef{client.Doc("Books")})
 
 	if err != nil {
 		log.Print("Error retrieving books")
+		fmt.Fprint(w, "Unable to retrieve books")
+		return
 	}
 
-	return books
+	fmt.Fprint(w, books)
 }
 
-func main() {
-	books := getBooks()
-
-	fmt.Print(books)
+func init() {
+	functions.HTTP("GetBooks", getBooks)
 }
