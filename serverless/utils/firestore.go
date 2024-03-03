878eaecb57d1df45f1f313ddd85863846795907f
mrewers <hello@marek.dev>
2024-03-02 22:50:18 -0500

    refactor: break out DB connection into a helper function

--- serverless/utils/firestore.go
+++ serverless/utils/firestore.go
@@ -7,18 +7,33 @@ import (
 	"cloud.google.com/go/firestore"
 )
 
+// openFirestoreConn connects to the Firestore database.
+func openFirestoreConn(ctx context.Context) (*firestore.Client, error) {
+	var client *firestore.Client
+	var err error
+
+	client, err = firestore.NewClient(ctx, os.Getenv("PROJECT_ID"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer client.Close()
+
+	return client, nil
+}
+
+// FirestoreGetAll retrieves all the items from the provided collection.
 func FirestoreGetAll(collection string) ([]*firestore.DocumentSnapshot, error) {
 	var docs []*firestore.DocumentSnapshot
 
 	ctx := context.Background()
 
-	client, err := firestore.NewClient(ctx, os.Getenv("PROJECT_ID"))
+	client, err := openFirestoreConn(ctx)
 
 	if err != nil {
-		return docs, err
+		panic(err.Error())
 	}
-
-	defer client.Close()
 
 	docs, err = client.GetAll(ctx, []*firestore.DocumentRef{client.Doc(collection)})
 
