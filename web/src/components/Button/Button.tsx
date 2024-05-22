ece5f938f434de8546df2e77f5cd375254242c87
mrewers <hello@marek.dev>
2024-05-22 09:43:03 -0400

    feat: allow datasets to be set on Buttons

--- web/src/components/Button/Button.tsx
+++ web/src/components/Button/Button.tsx
@@ -5,6 +5,7 @@ import s from './Button.module.scss';
 interface IButton {
   readonly classes?: string;
   readonly color?: 'accent' | 'dark' | 'light' | 'plain' | 'white';
+  readonly data?: { name: string, value: string }[];
   readonly disabled?: boolean,
   readonly label: string;
   readonly onClick?: (e: MouseEvent) => void;
@@ -18,19 +19,38 @@ const Button: Component<IButton> = (props) => {
     ? props.onClick
     : onClickFallback;
 
+  /**
+   * Iterates through and array of name-value pairs that
+   * should be converted into dataset properties.
+   * @returns An object containing the resulting dataset.
+   */
+  const dataIds = () => {
+    const dataProps = {} as {[key: string]: string};
+
+    if (props.data) {
+      props.data.forEach( i => {
+        dataProps[`data-${i.name}`] = i.value;
+      } );
+    };
+
+    return dataProps;
+  };
+
   return (
-  <button
-    class={
-      `${s.button}
-       ${s[props.color ?? 'light']}
-       ${props.classes ?? ''}`
-    }
-    disabled={props.disabled || false}
-    type={props.type ?? 'button'}
-    onClick={(e) => onClickFunc(e)} 
-  >
-    {props.label}
-  </button>
-)};
+    <button
+      class={
+        `${s.button}
+        ${s[props.color ?? 'light']}
+        ${props.classes ?? ''}`
+      }
+      disabled={props.disabled || false}
+      type={props.type ?? 'button'}
+      onClick={(e) => onClickFunc(e)}
+      {...dataIds()}
+    >
+      {props.label}
+    </button>
+  )
+};
 
 export default Button;
