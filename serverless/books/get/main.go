d36b2a0965c7974690fdbb956cd100457a3b1d5a
mrewers <hello@marek.dev>
2024-05-23 09:46:05 -0400

    feat: send the acquired on date in the format expected by the client

--- serverless/books/get/main.go
+++ serverless/books/get/main.go
@@ -1,8 +1,11 @@
 package getbooks
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"reflect"
+	"time"
 
 	_ "github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
@@ -18,7 +21,28 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 		log.Print("Error retrieving books")
 	}
 
-	utils.SendResponse(books, w)
+	var normalized []map[string]interface{}
+
+	for _, book := range books {
+		if book["dateAcquired"] != nil {
+			dateType := reflect.TypeOf(book["dateAcquired"])
+
+			fmt.Println(dateType.String())
+			// t := book["dateAcquired"].(time.Time).Format("2006-01-02")
+			// t, err := time.Parse(time.Layout, book["dateAcquired"].(string))
+
+			// if err != nil {
+			// 	log.Printf("Unable to parse the date for %s", book["dateAcquired"])
+			// 	log.Print(err.Error())
+			// }
+
+			book["dateAcquired"] = book["dateAcquired"].(time.Time).Format("2006-01-02")
+		}
+
+		normalized = append(normalized, book)
+	}
+
+	utils.SendResponse(normalized, w)
 }
 
 func init() {
--- serverless/utils/book.go
+++ serverless/utils/book.go
@@ -9,17 +9,31 @@ type Book struct {
 	DateAcquired string `json:"dateAcquired,omitempty"`
 	DateCreated  string `json:"dateCreated,omitempty"`
 	DateModified string `json:"dateModified,omitempty"`
+	Retired      bool   `json:"retired,omitempty"`
 	Title        string `json:"title,omitempty"`
 }
 
 // AddBook creates a Firestore document corresponding to the given Book.
 func (b Book) AddBook() string {
+	var acquired time.Time
+
 	ts := time.Now()
 
+	// Convert the provided acquisition date into a timestamp.
+	da, err := time.Parse("2006-01-02", b.DateAcquired)
+
+	if err != nil {
+		log.Println(err.Error())
+		acquired = ts
+	} else {
+		acquired = da
+	}
+
 	doc := map[string]interface{}{
-		"dateAcquired": b.DateAcquired,
+		"dateAcquired": acquired,
 		"dateCreated":  ts,
 		"dateModified": ts,
+		"retired":      b.Retired,
 		"title":        b.Title,
 	}
 
