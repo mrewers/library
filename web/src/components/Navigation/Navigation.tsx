edf4554beffda991d517793bd296ee054798ff7a
mrewers <hello@marek.dev>
2024-06-06 17:03:45 -0400

    feat: indicate when a link in the navigation is active

--- web/src/components/Navigation/Navigation.tsx
+++ web/src/components/Navigation/Navigation.tsx
@@ -1,43 +1,50 @@
-import type { Component } from 'solid-js';
-import { Show } from 'solid-js';
-import { A } from '@solidjs/router';
+import { createMemo, Show } from 'solid-js';
+import { A, useLocation } from '@solidjs/router';
 
 import Button from 'components/Button/Button';
 
 import { logout, login, isLoggedIn } from 'utils/auth';
 
+import type { Component } from 'solid-js';
+
 import s from './Navigation.module.scss';
 
-const Navigation: Component = () => (
-  <nav class={s.nav}>
-    <ul class={s.items}>
-      <li>
-        <A class={s.link} href="/">
-          Home
-        </A>
-      </li>
-      <Show when={isLoggedIn()}>
+const Navigation: Component = () => {
+  const location = useLocation();
+
+  const pathname = createMemo(() => location.pathname);
+
+  return (
+    <nav class={s.nav}>
+      <ul class={s.items}>
         <li>
-          <A class={s.link} href="/add">
-            Add New
+          <A class={`${s.link} ${pathname() === "/" ? s.active : ""}`} href="/">
+            Home
           </A>
         </li>
+        <Show when={isLoggedIn()}>
+          <li>
+            <A class={`${s.link} ${pathname() === "/add" ? s.active : ""}`} href="/add">
+              Add New
+            </A>
+          </li>
+        </Show>
+        <li>
+          <A class={`${s.link} ${pathname() === "/retired" ? s.active : ""}`} href="/retired">
+            Jettisoned
+          </A>
+        </li>
+      </ul>
+      <Show
+        when={isLoggedIn()}
+        fallback={
+          <Button color="plain" label="Log In" type="button" onClick={(): void => login()} />
+        }
+      >
+        <Button color="plain" label="Log Out" type="button" onClick={(): void => logout()} />
       </Show>
-      <li>
-        <A class={s.link} href="/retired">
-          Jettisoned
-        </A>
-      </li>
-    </ul>
-    <Show
-      when={isLoggedIn()}
-      fallback={
-        <Button color="plain" label="Log In" type="button" onClick={(): void => login()} />
-      }
-    >
-      <Button color="plain" label="Log Out" type="button" onClick={(): void => logout()} />
-    </Show>
-  </nav>
-);
+    </nav>
+  )
+};
 
 export default Navigation;
