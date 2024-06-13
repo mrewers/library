022c28f3694fc3db4d38cab37c98f332d9ee1b2c
mrewers <hello@marek.dev>
2024-06-13 19:00:34 -0400

    refactor: use aria property rather than rather than class to manage active styling on navigation link

--- web/src/components/Navigation/Navigation.tsx
+++ web/src/components/Navigation/Navigation.tsx
@@ -1,5 +1,5 @@
-import { createMemo, Show } from 'solid-js';
-import { A, useLocation } from '@solidjs/router';
+import { Show } from 'solid-js';
+import { A } from '@solidjs/router';
 
 import Button from 'components/Button/Button';
 
@@ -9,42 +9,51 @@ import type { Component } from 'solid-js';
 
 import s from './Navigation.module.scss';
 
-const Navigation: Component = () => {
-  // Identify the current page in order to highlight the active nav link.
-  const location = useLocation();
-  const pathname = createMemo(() => location.pathname);
-
-  return (
-    <nav class={s.nav} role="navigation">
-      <ul class={s.items}>
+/**
+ * A navigation element with a list of links to the main pages on the site.
+ * Appears in the page header on tablet and above sized screens.
+ * @returns A SolidJS JSX component.
+ */
+const Navigation: Component = () => (
+  <nav class={s.nav} role="navigation">
+    <ul class={s.items}>
+      <li class={s.item}>
+        <A class={s.link} href="/">
+          Home
+        </A>
+      </li>
+      <Show when={isLoggedIn()}>
         <li class={s.item}>
-          <A class={`${s.link} ${pathname() === "/" ? s.active : ""}`} href="/">
-            Home
+          <A class={s.link} href="/add">
+            Add New
           </A>
         </li>
-        <Show when={isLoggedIn()}>
-          <li class={s.item}>
-            <A class={`${s.link} ${pathname() === "/add" ? s.active : ""}`} href="/add">
-              Add New
-            </A>
-          </li>
-        </Show>
-        <li class={s.item}>
-          <A class={`${s.link} ${pathname() === "/retired" ? s.active : ""}`} href="/retired">
-            Jettisoned
-          </A>
-        </li>
-      </ul>
-      <Show
-        when={isLoggedIn()}
-        fallback={
-          <Button color="link" label="Log In" type="button" onClick={() => login()} />
-        }
-      >
-        <Button color="link" label="Log Out" type="button" onClick={() => logout()} />
       </Show>
-    </nav>
-  )
-};
+      <li class={s.item}>
+        <A class={s.link} href="/retired">
+          Jettisoned
+        </A>
+      </li>
+    </ul>
+    <Show
+      when={isLoggedIn()}
+      fallback={
+        <Button
+          color="link"
+          label="Log In"
+          type="button"
+          onClick={() => login()}
+        />
+      }
+    >
+      <Button
+        color="link"
+        label="Log Out"
+        type="button"
+        onClick={() => logout()}
+      />
+    </Show>
+  </nav>
+);
 
 export default Navigation;
